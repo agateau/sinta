f21075ee4ee2798f80919d4e2b08d1d02fbb3def
Aurélien Gâteau <mail@agateau.com>
2019-05-19 18:33:14 +0200

    Split into an injectable lib and a test app

---
+++ injeqt/Injector.cpp
@@ -0,0 +1,14 @@
+#include "ScriptRunner.h"
+
+#include <QCoreApplication>
+#include <QTimer>
+
+void setupScriptRunner() {
+    QString scriptPath = qgetenv("INJECT_SCRIPT");
+    auto* scriptRunner = new ScriptRunner;
+    QTimer::singleShot(0, [scriptRunner, scriptPath] {
+        scriptRunner->runScript(scriptPath);
+    });
+}
+
+Q_COREAPP_STARTUP_FUNCTION(setupScriptRunner)
